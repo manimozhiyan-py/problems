c2efae2b5976d26b5a609ffe81611030e3bfe5f1
manimozhiyan <135946674+manimozhiyan-py@users.noreply.github.com>
2026-03-24 10:20:24 +0530

    Implement palindrome check using a for loop
    Added a function to check for palindromes using a for loop.

--- string_1.py
+++ string_1.py
@@ -1,4 +1,12 @@
 def palindrome(string):
     return string == string[::-1]
 
+def using_for(string):
+    no_of_char = len(string) -1
+    for index in range(int(len(string)/2)):
+        if string[index] == string[no_of_char - index]:
+            return False 
+    return True
+        
+        
 print(palindrome("tenet"))
